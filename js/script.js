83481b14e14d7b289290847ee4d2c8f9efe603b0
Santiago S. J. Madrid <santiago.madrid14@gmail.com>
2023-10-01 20:22:05 -0300

    limpiando archivos de script

--- js/script.js
+++ js/script.js
@@ -1,194 +1,128 @@
-let titlePage = document.querySelector('h1');
-titlePage.textContent = "Listado de productos";
+'use strict';
 
-fetch("productos.json")
-  .then(response => response.text())
-  .then(productos => init(productos));
-
-if(localStorage.length){
-  localStorage.clear();
-}
-
-function init(p) {
-  let arrayOfProducts = JSON.parse(p);
-  const contentList = document.getElementById("content-table");
-  try{
-    contentList.append(createTable(arrayOfProducts));
+window.onload = (event) => {
+  if(window.location == "http://192.168.0.9:8080/productos/"){
+    //alert('ok');
+    event.stopPropagation();
   }
-  catch(err){
-    alert(err);
+  else{
+    //loadDetailPage();
   }
-}
-function createPageOfProduct(item, callback){
-  const firstParagraph = document.body.querySelector("p");
+};
 
-  titlePage.textContent = item.codigo;
-  titlePage.classList.add("producto_titulo");
-  firstParagraph.hidden = true;
-  window["content-table"].innerHTML = "";
-  const section = document.createElement("section");
-  section.innerHTML = 
-    `<div class="row">
-      <div class="col-12">
-      <h2>
-	Descripción
-      </h2>
-      </div>
-    </div>
-    <div class="row producto">
-      <div class="col-6">
-	<p class="producto_descripcion">
-	  ${item.descripcion}
-	</p>
-	<p class="producto_descripcion">
-	  ${item["descripcion-extra"]}
-	</p>
-      </div>
-      <div class="col-6">
-	<img height=196 width=256
-	src="${item.imagen}" alt""
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12">
-      <h3 class="precio"> ${item.precio} </h3>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12">
-      <ul class="puntuacion">
-      </ul>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12">
-      <a href="" class="btn_back">
-	<img height=32 width=32 class="arrow_left"
-	src="icons/left-arrow.png" alt="Fecha izquierda para volver atrás"/>
-	<span class="arrow-left">
-	Volver
-	</span>
-      </a>
-      </div>
-    </div> `;
-  document.body.append(section);
-  section.onload = callback();
-  
-  
-  //setTimeout(() => {
-  //  window["content-item"].append(h2);
-  //}, 1000);
+const loadJson = (url) => {
 
-  /*
-  product_description.innerHTML = 
-  `<p class="producto_descripcion"> ${item.descripcion}  </p>
-   <p class="producto_descripcion"> ${item["descripcion-extra"]} </p>`;
-*/
+  fetch(url)
+    .then(response => response.json())
+    .then(products => {
+      for(let item in products){
+	generateHTML(products[item]);
+      }
+    });
+};
+
+const generateHTML = function(product){
+  const tbody = document.querySelector('table tbody');
+  let row;
+
+  if(window.location == "http://192.168.0.9:8080/productos/"){  
+    row = createRow(product);
+  }
+  else if(window.location == "http://192.168.0.9:8080/productos/detalle.html"){
+    loadDetailPage(product);
+    return;
+  }
+
+  tbody.append(row);
 }
 
-function createTable(products){
-  const table = document.createElement("table");
-  const caption = document.createElement("caption");
-  const table_header = createTableHeader();
+const redirect = (str) => {
+  const BASE_URL = "http://192.168.0.9:8080/productos/";
+  window.location = BASE_URL + str;
+}
 
-  for(let i = 0; i <= products.length - 1; i++) {
-    const row = document.createElement("tr");
-    for(let td = 1; td <= 3; td++){
-      const row_cell = document.createElement("td");
-	switch(td){
-	case 1:
-	  row_cell.textContent = products[i].codigo;
-	  break;
-	case 2:
-	  row_cell.textContent = products[i].descripcion;
-	  break;
-	case 3:
-	  row_cell.innerHTML = `<img height=32 width=32
-				src="icons/acercarse.png" alt="lupa para examinar"/>`;
-	  row_cell.addEventListener("click", () => {
-	    createPageOfProduct(products[i], () => {
-	      const score = document.querySelector(".puntuacion");
-	      let stop = products[i].puntuacion.length;
-	      for (let i = 0; i < stop; i++){
-		//alert(i);
-		const star = document.createElement("li");
-		star.innerHTML = `<i class="fa-solid fa-star star-gold"></i>`;
-		score.append(star);
-	      }
-	    });  
-	    localStorage.setItem(products[i].codigo, products[i]);
-	  });
-	  break;
-	}
-      row.append(row_cell);
+function createRow(product) {
+  const row = document.createElement('tr');
+  const template = 
+    `<td> ${product.codigo} </td>
+    <td> ${product.descripcion} </td>
+    <td> 
+      <img width=32 height=32
+	src="icons/acercarse.png" alt="Lupa para examinar">
+    </td>`;
+  row.innerHTML = template;
+  row.cells[2].addEventListener('click', () => {
+    if(localStorage.getItem('product.codigo') == null){
+      localStorage.setItem(product.id, JSON.stringify(product));
     }
+    redirect("detalle.html");
 
-    caption.textContent = "Lista de productos";
-    table.append(caption);
-    table.append(table_header);
-    table.append(row);
-
-  }
-
-  return table;
+  });
+  return row;
 }
 
-export function createTableHeader() {
-  const thead = document.createElement("thead");
-  const header_row = document.createElement("tr");
-  const table_headers = [];
-   
-  for(let i = 0; i <= 2; i++){
-    table_headers[i]= document.createElement("th");
-    table_headers[i].setAttribute('scope','row');
-  }
+const loadDetailPage = function(product) {
+  let item = localStorage.getItem(product.id);
 
-  table_headers[0].textContent = "Productos";
-  table_headers[1].textContent = "Descripcion";
-  table_headers[2].textContent = "Ver más";
+  if(item == null){
+    return;
+  } 
 
-  for(let i = 0; i <= 2; i++){
-    header_row.append(table_headers[i]);
-  }
+  item = JSON.parse(item);
+  localStorage.removeItem(item.id);
+
+  const title = document.querySelector('title');
+  const description = document.querySelector('#description');
+  const product_img = document.querySelector('#img');
+  const img = document.createElement('img');
+  const price = document.querySelector('#price');
+  const score = document.querySelector("#score");
+  const ul = document.createElement('ul');
+  const button = document.querySelector('.btn_back');
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    redirect("");
+  });
+
+  const heading = createHeading(item.codigo);
+  heading.classList.add('producto_titulo');
+  document.body.prepend(heading);
   
-  thead.append(header_row);
-  
-  return thead;
-}
-/* Si agrego esta funcion se rompe
-function createTableBody(products){
+  title.textContent = `Detalle ${item.id}`;
+  description.innerHTML = `<p class="producto_descripcion"> ${item.descripcion} </p>
+        		   <p class="producto_descripcion"> ${item["descripcion-extra"]} </p>`;
+  img.src = item.imagen ? item.imagen : "img/no-img.png";
+  img.setAttribute('width', 256);
+  img.setAttribute('height', 256);
+  product_img.append(img);
 
-  for(let i = 0; i <= products.length - 1; i++) {
-    const row = document.createElement("tr");
-    for(let td = 1; td <= 3; td++){
-      const row_cell = document.createElement("td");
-	switch(td){
-	case 1:
-	  row_cell.textContent = products[i].codigo;
-	  break;
-	case 2:
-	  row_cell.textContent = products[i].descripcion;
-	  break;
-	case 3:
-	  row_cell.innerHTML = `<img height=32 width=32
-				src="icons/acercarse.png" alt="lupa para examinar"/>`;
-	  row_cell.addEventListener("click", () => {
-	    createPageOfProduct(products[i], () => {
-	      const score = document.querySelector(".puntuacion");
-	      let stop = products[i].puntuacion.length;
-	      for (let i = 0; i < stop; i++){
-		//alert(i);
-		const star = document.createElement("li");
-		star.innerHTML = `<i class="fa-solid fa-star star-gold"></i>`;
-		score.append(star);
-	      }
-	    });  
-	    localStorage.setItem(products[i].codigo, products[i]);
-	  });
-	  break;
-	}
-      row.append(row_cell);
-    }
+  price.innerHTML = `<h3> Precio ${item.precio} </h3>`;
 
+  for (let i = 0; i < item.valoracion.length; i++){
+    const star = document.createElement("li");
+
+    star.innerHTML = `<i class="fa-solid fa-star star-gold"></i>`;
+    ul.append(star);
+  }
+
+  score.append(ul);
+
+};
+
+const createHeading = (str) => {
+  let h1 = document.createElement('h1');
+
+  h1.textContent = str;
+  return h1;
+};
+
+try {
+  loadJson("productos.json");
 }
-*/ 
+
+catch(err){
+  alert(err);
+}
+
+
